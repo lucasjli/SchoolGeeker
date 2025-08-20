94adc8d03b9a90a48ea2f5f1e7ece77927d7cf64
lucas <lijie.lucas@hotmail.com>
2025-08-20 16:53:14 +1200

    Build UserReview.cs

---
+++ Models/UserReview.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SchoolGeeker.Models
+{
+    public class UserReview
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        public int UserID { get; set; }
+
+        [Required]
+        public int SchoolID { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string Comments { get; set; }
+
+        [Required]
+        public bool IsLiked { get; set; }
+
+        public bool IsApproved { get; set; }
+
+        public DateTime? DateSubmitted { get; set; }
+    }
+}
