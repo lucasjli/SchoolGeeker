9e7105606dff1960cd0463ba8eb2280f6c0c75db
lucas <lijie.lucas@hotmail.com>
2025-09-23 02:20:17 +1200

    Add proxy.

---
+++ frontend/vite.config.js
@@ -0,0 +1,17 @@
+﻿import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    plugins: [react()],
+    server: {
+        port: 63342,
+        open: true,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:5227',
+                changeOrigin: true,
+                secure: false,
+            }
+        }
+    }
+});
